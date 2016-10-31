24ac7c64c6942754e3d78fdc4832175fe2a4025c
molenzwiebel <info@thijsmolendijk.nl>
2016-10-31 21:07:00 +0100

    Fix the ember-component hook so that it always works, regardless of plugin loading order.

--- src/hook-providers/ember-component.ts
+++ src/hook-providers/ember-component.ts
@@ -5,6 +5,7 @@ import Ace from "../ace";
 import Promise = require("bluebird");
 
 export const NAME = "ember-component";
+const HOOKED = (<any>window).Symbol("ace-ember-component-hooked");
 
 type Callback = (Ember: any, args: any[]) => any;
 const HOOKS: { matcher: string, fun: Callback }[] = [];
@@ -31,30 +32,41 @@ export function register(fun: Callback, matcher: string) {
  * Initializes this hook provider by wrapping the various instances of `Ember.Component.extend`.
  */
 export function initialize(ace: Ace) {
-    // At the time of writing, 1.12.1 is also present in the LCU.
-    // Since only the patcher uses it, and it warns about deprecation,
-    // I opted not to include it in the hook.
-    ace.getBuiltinApi("rcp-fe-ember-libs").then(api => Promise.all([
-        api.getEmber("2.2.0"),
-        api.getEmber("2.4.5")
-    ])).then(embers => {
-        embers.forEach(Ember => {
-            wrap_method(Ember.Component, "extend", function(original, args) {
-                // Find the classNames component, in case there were mixins present.
-                const name = (<any[]>args).filter(x => (typeof x === "object") && x.classNames && Array.isArray(x.classNames)).map(x => x.classNames.join(" "));
-                let res = original(...args);
+    ace.getBuiltinApi("rcp-fe-ember-libs").then(api => {
+        // We need to do a little dance here to make sure we hook before the first invocation.
+        // Since rcp-fe-ember-libs is async, we cannot guarantee that we are the first to receive
+        // the Ember instance if we simply hook the result of api.getEmber. Instead, we need to
+        // make sure we modify the Ember instance before we return it to the plugin requesting it.
+        wrap_method(api, "getEmber", function(original, args) {
+            const res: Promise<any> = original(...args);
 
-                if (name.length) {
-                    HOOKS.filter(x => x.matcher === name[0]).forEach(hook => {
-                        const hookResult = hook.fun(Ember, args);
-                        if (hookResult) {
-                            res = res.extend(hookResult);
-                        }
-                    });
-                }
+            return res.then(Ember => {
+                // No point in hooking twice.
+                if (Ember[HOOKED]) return Ember;
 
-                return res;
+                Ember[HOOKED] = true;
+                hookEmber(Ember);
+                return Ember;
             });
         });
+    });
+}
+
+function hookEmber(Ember: any) {
+    wrap_method(Ember.Component, "extend", function(original, args) {
+        // Find the classNames component, in case there were mixins present.
+        const name = (<any[]>args).filter(x => (typeof x === "object") && x.classNames && Array.isArray(x.classNames)).map(x => x.classNames.join(" "));
+        let res = original(...args);
+
+        if (name.length) {
+            HOOKS.filter(x => x.matcher === name[0]).forEach(hook => {
+                const hookResult = hook.fun(Ember, args);
+                if (hookResult) {
+                    res = res.extend(hookResult);
+                }
+            });
+        }
+
+        return res;
     });
 }
