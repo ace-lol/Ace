0ac5aebc56d084054f309c9259b12f55dbaa072f
molenzwiebel <info@thijsmolendijk.nl>
2016-11-11 09:48:56 +0100

    Finish resizing, it works fairly decent now. Resolves #6. The unfortunate thing is that `transform: scale` causes fairly blurry text rendering, but there's nothing to be done about that. Also interesting is that you cannot apply `transform: scale` to the layer manager, because it messes up the absolute positioning. Still need to find something for that.

--- src/plugins/resize/index.ts
+++ src/plugins/resize/index.ts
@@ -10,13 +10,17 @@ export default (<PluginDescription>{
     description: "Allows you to resize the client, as long as it keeps its standard aspect ratio.",
     setup() {
         // UIKit layer manager puts something in document.body. We want to insert _after_ that, so that we are on top.
+        // TODO(molenzwiebel): Save the scale somewhere.
+        // TODO(molenzwiebel): Scale the lol-uikit-layer-manager without losing the absolute positioning.
         this.postinit("rcp-fe-lol-uikit", () => {
             const resizer = document.createElement("div");
             resizer.className = "resize-handle";
             document.body.appendChild(resizer);
 
+            const viewport = document.getElementById("rcp-fe-viewport-root")!;
+
             // State variables for during the dragging.
-            let [initialWidth, initialHeight, currentWidth, currentHeight, currentX, currentY, scale] = [0, 0, 0, 0, 0, 0, 0.0];
+            let [initialWidth, initialHeight, currentWidth, currentHeight, currentX, currentY] = [0, 0, 0, 0, 0, 0];
 
             const dragMove = (event: MouseEvent) => {
                 let newHeight = 0;
@@ -31,7 +35,11 @@ export default (<PluginDescription>{
                 }
 
                 // Use builtin riot apis to resize the window.
-                (<any>window).riotInvoke({ request:JSON.stringify({ name:"Window.ResizeTo", params: [newWidth, newHeight] }) });
+                (<any>window).riotInvoke({ request: JSON.stringify({ name:"Window.ResizeTo", params: [newWidth, newHeight] }) });
+
+                viewport.style.transform = `scale(${newWidth / 1280}) translateZ(0)`;
+                viewport.style.transformOrigin = "0 0";
+                viewport.style.webkitFilter = "blur(0)"; // Seems to fix a bit of the blurryness.
             };
 
             const dragStart = (event: MouseEvent) => {
@@ -49,12 +57,6 @@ export default (<PluginDescription>{
 
             const dragEnd = (event: MouseEvent) => {
                 window.removeEventListener("mousemove", dragMove);
-
-                // TODO(molenzwiebel): seems to change the scale of the client.
-                //const http = new XMLHttpRequest();
-                //http.open("PATCH", "/lol-settings/v1/local/video", true);
-                //http.setRequestHeader("Content-type", "application/json");
-                //http.send(`{"schemaVersion": 1, "data": {\"ZoomScale\": ${ scale }}}`);
             };
 
             resizer.addEventListener("mousedown", dragStart);
