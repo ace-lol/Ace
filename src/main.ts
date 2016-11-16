65ad9574a3715fbeebebee89db90182a44ad52e9
molenzwiebel <info@thijsmolendijk.nl>
2016-11-16 22:16:26 +0100

    Start keeping track of the current Ace version.

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@
 
 import Ace from "./ace";
 
+/**
+ * Version metadata. This is both shown in the about screen and used as a version check.
+ */
+window["ACE_VERSION"] = "0.1.0";
+window["ACE_BUILD"] = 1;
+
 (<any>window).Ace = new Ace();
