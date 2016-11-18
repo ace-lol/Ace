70ff2ee3679cde318ddfc4ee9ef3acbadc9bc83f
molenzwiebel <info@thijsmolendijk.nl>
2016-11-18 23:43:25 +0100

    Add changelog display after an update.

--- src/plugins/changelog/index.ts
+++ src/plugins/changelog/index.ts
@@ -1,15 +1,20 @@
 "use strict";
 
 import { PluginDescription } from "../../plugin";
+import { simple_promise_fetch } from "../../util";
 import SettingsAPI from "../settings/api";
 import semver = require("semver");
+import marked = require("marked");
 
 import "./welcome/style";
 import WELCOME_HTML = require("./welcome/index.html");
 
+import "./changelog/style";
+import CHANGELOG_HTML = require("./changelog/index.html");
+
 interface ChangelogSettings {
-    didShowWelcome: boolean | null;
-    previousVersion: string | null;
+    didShowWelcome: boolean;
+    previousVersion: string;
 }
 
 export default (<PluginDescription>{
@@ -37,6 +42,31 @@ export default (<PluginDescription>{
                     state.didShowWelcome = true;
                     settings.mergeSettings({ changelog: state });
                     settings.save();    
+                });
+                return;
+            }
+
+            // If we updated, show the changelog and update the previousVersion prop.
+            if (semver.gt(window.ACE_VERSION, state.previousVersion)) {
+                state.previousVersion = window.ACE_VERSION;
+                settings.mergeSettings({ changelog: state });
+                settings.save(); 
+
+                // Fetch changelog.
+                simple_promise_fetch("https://api.github.com/repos/ace-lol/ace/releases").then(json => {
+                    const data: any[] = JSON.parse(json);
+                    const release: { body: string } = data.filter(x => x.tag_name === window.ACE_VERSION)[0];
+                    return release.body;
+                }).then(markdownBody => {
+                    const customRenderer = new marked.Renderer();
+                    customRenderer.link = (href, title, contents) => `<a href="${href}" target="blank">${contents}</a>`;
+
+                    showFullpageBackdrop(
+                        uikit,
+                        CHANGELOG_HTML
+                            .replace("CHANGELOG", marked(markdownBody, { renderer: customRenderer }))
+                            .replace("VERSION", window.ACE_VERSION)
+                    );
                 });
             }
         });
